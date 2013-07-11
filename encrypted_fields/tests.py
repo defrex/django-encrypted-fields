fc876cb727137a54f3287133e782586bb855d95c
Aron Jones <aron.jones@gmail.com>
2013-07-11 17:28:21 -0400

    initial commit, project setup

---
+++ encrypted_fields/tests.py
@@ -0,0 +1,2 @@
+
+from django.test import TestCase
---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+from setuptools import setup
+import re
+import os
+import sys
+
+
+name = 'Django Encrypted Fields'
+package = 'django-encrypted-fields'
+description = ''
+url = 'http://github.com/defrex/django-encrypted-fields/'
+author = 'Aron Jones'
+author_email = 'aron.jones@gmail.com'
+license = 'BSD'
+install_requires = []
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("^__version__ = ['\"]([^'\"]+)['\"]", init_py, re.MULTILINE).group(1)
+
+
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    print("You probably want to also tag the version now:")
+    print("  git tag -a {0} -m 'version {0}'".format(get_version(package)))
+    print("  git push --tags")
+    sys.exit()
+
+
+setup(
+    name=name,
+    version=get_version(package),
+    url=url,
+    license=license,
+    description=description,
+    author=author,
+    author_email=author_email,
+    packages=['encrypted_fields'],
+    install_requires=install_requires
+)
