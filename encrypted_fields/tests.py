16c809ea75bbd77f1d73612e4ec60cddbd798213
Aron Jones <aron.jones@gmail.com>
2017-01-26 11:07:22 -0500

    change to py3 assertEqual

--- encrypted_fields/tests.py
+++ encrypted_fields/tests.py
@@ -120,7 +120,7 @@ class FieldTest(TestCase):
         model.save()
 
         plaintext = self.get_db_value('decrypt_only', model.id)
-        self.assertEquals(plaintext, known_plaintext)
+        self.assertEqual(plaintext, known_plaintext)
 
     def test_decrypt_only_plaintext(self):
         known_plaintext = 'I am so plain and ordinary'
@@ -129,7 +129,7 @@ class FieldTest(TestCase):
         model.save()
 
         plaintext = self.get_db_value('decrypt_only', model.id)
-        self.assertEquals(plaintext, known_plaintext)
+        self.assertEqual(plaintext, known_plaintext)
 
     def test_char_field_encrypted(self):
         plaintext = 'Oh hi, test reader!'
